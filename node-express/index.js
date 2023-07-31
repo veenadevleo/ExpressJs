c2f134ef1665d64bc897126dd7ee166f9ebc0da5
Veena M <veena.m@zerone-consulting.com>
2023-07-31 16:20:52 +0530

    Assignment1 completed

--- node-express/index.js
+++ node-express/index.js
@@ -3,6 +3,10 @@ const http = require('http');
 const morgan = require('morgan');
 const bodyParser=require('body-parser');
 
+const dishRouter=require('./routes/dishRouter')
+const promotionRouter=require('./routes/promotionRouter')
+const leaderRouter=require('./routes/leaderRouter')
+
 const hostname='localhost';
 const port=4000;
 
@@ -10,50 +14,9 @@ const app=express();
 app.use(morgan('dev'));
 app.use(bodyParser.json());
 
-app.all('/dishes', (req, res, next) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain'); // Corrected method name here
-    next();
-});
-
- app.get('/dishes',(req,res,next)=>{
-    res.end('will send all the dishes to you!');
-});
-
-app.post('/dishes',(req,res,next)=>{
-    res.end('will add dish :'+req.body.name+
-    'with details'+req.body.description);
-});
-
-app.put('/dishes',(req,res,next)=>{
-    res.statusCode=403;
-    res.end('PUT operation not suported on /dishes');
-});
-
-app.delete('/dishes',(req,res,next)=>{
-    res.end('deleting all dishes');
-});
-
-app.get('/dishes/:dishId',(req,res,next)=>{
-    res.end('will send details of dish :'
-    +req.params.dishId+'to you');
-});
-
-app.post('/dishes/:dishId',(req,res,next)=>{
-    res.statusCode=403;
-    res.end('POST operation not suported on /dishes/'
-    +req.params.dishId);
-});
-
-app.put('/dishes/:dishId',(req,res,next)=>{
-    res.write('updating the dish:'+req.params.dishId+'\n');
-    res.end('will update the dish :'+req.body.name+
-    'with details:'+req.body.description);
-});
-
-app.delete('/dishes/:dishId',(req,res,next)=>{
-    res.end('deleting dishes:'+req.params.dishId);
-});
+app.use('/dishes',dishRouter);
+app.use('/promotions',promotionRouter);
+app.use('/leaders',leaderRouter);
 
 app.use(express.static(__dirname+'/public'));
 
