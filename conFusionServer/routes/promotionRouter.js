7e78d7b30efad052e043f2e6dd5c78d0d30ee889
Veena M <veena.m@zerone-consulting.com>
2023-07-31 19:13:52 +0530

    express generator

---
+++ conFusionServer/routes/promotionRouter.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const promotionRouter = express.Router();
+
+promotionRouter.use(bodyParser.json());
+
+promotionRouter.route('/')
+  .all((req, res, next) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    next();
+  })
+  .get((req, res, next) => {
+    res.end('Will send all the promotions to you!');
+  })
+  .post((req, res, next) => {
+    res.end('Will add promotions: ' + req.body.name +
+      ' with details: ' + req.body.description);
+  })
+  .put((req, res, next) => {
+    res.statusCode = 403;
+    res.end('PUT operation not supported on /prootions');
+  })
+  .delete((req, res, next) => {
+    res.end('Deleting all promotions');
+  });
+
+// Adding routes with ID
+promotionRouter.route('/:promotionId')
+  .get((req, res, next) => {
+    res.end('Will send details of the promotion: ' + req.params.promotionId + ' to you!');
+  })
+  .post((req, res, next) => {
+    res.statusCode = 403;
+    res.end('POST operation not supported on /promotions/' + req.params.promotionId);
+  })
+  .put((req, res, next) => {
+    res.write('Updating the promotion: ' + req.params.promotionId + '\n');
+    res.end('Will update the promotion: ' + req.body.name +
+      ' with details: ' + req.body.description);
+  })
+  .delete((req, res, next) => {
+    res.end('Deleting promotion: ' + req.params.promotionId);
+  });
+
+module.exports = promotionRouter;
