9838bf317bae45fd5701a38d8db899dd1f8f6dd8
Veena M <veena.m@zerone-consulting.com>
2023-08-01 17:24:10 +0530

    node-mongoose

---
+++ node-mongoosee/index.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+
+const Dishes= require('./models/dishes');
+
+const url='mongodb://localhost:27017/conFusion';
+const connect=mongoose.connect(url);
+
+connect.then((db)=>{
+    console.log('connected correctly to server');
+    
+    var newdish=Dishes({
+        name:'uthappizza',
+        description:'test'
+    });
+
+    newdish.save()
+        .then((dish)=>{
+            console.log(dish);
+
+            return Dishes.find({}).exec();
+        })
+        .then((dishes)=>{
+            console.log(dishes);
+
+            return Dishes.remove({});
+        })
+        .then(()=>{
+            return mongoose.connection.close();
+        })
+        .catch((err)=>{
+            console.log(err);
+        });
+});
